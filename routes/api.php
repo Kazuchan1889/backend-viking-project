80f419f417ac0d84872b1313618893d193fb2d5d
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-02 13:07:28 +0700

    change

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,5 @@ Route::delete('news/{id}', [NewsController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+require __DIR__.'/auth.php';
