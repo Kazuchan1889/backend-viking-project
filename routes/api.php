71945b4f5dcfaf472e963102043cdf5b6510a092
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-06-04 00:30:39 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('news', [NewsController::class, 'index']);
 Route::get('news/{id}', [NewsController::class, 'show']);
@@ -13,9 +12,3 @@ Route::delete('news/{id}', [NewsController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
-Route::middleware('auth:sanctum')->get('/logout', [AuthenticatedSessionController::class, 'destroy']);
-
-require __DIR__.'/auth.php';
