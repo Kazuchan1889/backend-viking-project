d0aa5bff6d2bcee98d4865b006612c048d91ba8b
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-06-03 13:45:48 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
+Route::get('news', [NewsController::class, 'index']);
+Route::get('news/{id}', [NewsController::class, 'show']);
+Route::post('news', [NewsController::class, 'store']);
+Route::put('news/{id}', [NewsController::class, 'update']);
+Route::delete('news/{id}', [NewsController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
