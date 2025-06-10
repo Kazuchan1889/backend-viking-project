929c4c3ea016a29a69d1842275a329001b334740
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-10 23:34:08 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
 
-    Route::get('/logout', [AuthenticatedSessionController::class, 'destroy']);
-
 Route::middleware('auth:sanctum')->get('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
