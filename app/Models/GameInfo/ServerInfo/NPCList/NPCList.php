b0d87929a1886a051512f538b84c3d8c9f3264d0
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-26 16:42:26 +0700

    Update NPCList.php

--- app/Models/GameInfo/ServerInfo/NPCList/NPCList.php
+++ app/Models/GameInfo/ServerInfo/NPCList/NPCList.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\GameInfo\MapInformation;
 
-class NpcList extends Model
+class NPCList extends Model
 {
     use HasFactory;
 
