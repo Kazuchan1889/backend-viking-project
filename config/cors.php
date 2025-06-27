f7d12bd004c04397e6d246d25645c9aa496fd951
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-06-27 18:17:52 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://vikingsf.vercel.app/')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://vikingsf.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
