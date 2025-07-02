8b9d92a29eea089f53cf8abc16b3152c2c2aa88d
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-07-02 11:15:03 +0700

    menolak csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
         ]);
 
         $middleware->alias([
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
