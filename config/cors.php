148493b5d0355356e1a8fbd171a2e2acc050c9e3
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-07-02 10:15:37 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://vikingsf.vercel.app'],
+    'allowed_origins' => ['https://vikingsf-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
